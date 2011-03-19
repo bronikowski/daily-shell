0eda6012b94a475873084a33b0ff521f4f2601f5
Emil Oppeln-Bronikowski <junkyard@bronikowski.com>
2011-03-19 09:27:25 +0100

    Dump MySQL, one file per database

---
+++ backup-mysql-by-db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USER='root'
+PASSWORD='sqlsucks'
+BACKUP='/tmp'
+
+databases=`mysql --user=$USER --password=$PASSWORD -e 'SHOW DATABASES;' | grep -Ev '(mysql|Database|information_schema)'`
+for db in $databases; do
+	echo -n "Dumping $db..."
+	mysqldump --user=$USER --password=$PASSWORD $db | bzip2 > $BACKUP/$db.bz2
+	echo "done"
+done
